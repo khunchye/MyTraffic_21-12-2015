ceadf973ee8ba5aeacd4f671b3ef068de668a97d
khunchye <sirichai_v@uih.co.th>
2015-12-21 16:41:32 +0700

    Finish day 1

--- app/src/main/java/com/example/sirichai_v/traffic/MainActivity.java
+++ app/src/main/java/com/example/sirichai_v/traffic/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.sirichai_v.traffic;
 
+import android.provider.LiveFolders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit ประกาศตัวแปล
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Wiget
+        BindWidget();
+
+
+
+    }   // Main Method
+
+    private void BindWidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
     }
-}
+
+}   // Main Class
+
