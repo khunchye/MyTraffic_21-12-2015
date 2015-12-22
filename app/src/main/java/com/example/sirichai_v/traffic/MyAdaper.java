c31f2ea486ead9ccf5247987d379fd095547f6dd
khunchye <sirichai_v@uih.co.th>
2015-12-22 13:43:10 +0700

    Create ListView

---
+++ app/src/main/java/com/example/sirichai_v/traffic/MyAdaper.java
@@ -0,0 +1,68 @@
+package com.example.sirichai_v.traffic;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by sirichai_v on 12/22/2015.
+ */
+public class MyAdaper extends BaseAdapter{
+
+    //Explicit
+    private Context objContext;
+    private String[] titleStrings, detailStrings;
+    private int[] inconInts;
+
+    public MyAdaper(Context objContext, String[] titleStrings, String[] detailStrings, int[] inconInts) {
+        this.objContext = objContext;
+        this.titleStrings = titleStrings;
+        this.detailStrings = detailStrings;
+        this.inconInts = inconInts
+
+    }   // Constructor
+
+    @Override
+    public int getCount() {
+        return titleStrings.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater objLayoutInflater = (LayoutInflater) objContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View objView = objLayoutInflater.inflate(R.layout.my_listview, parent, false);
+
+        //For Title
+
+        TextView titleTextView = (TextView) objView.findViewById(R.id.textView2);
+        titleTextView.setText(titleStrings[position]);
+
+        //For Detail
+        TextView detailTextView = (TextView) objView.findViewById(R.id.textView3)
+        detailTextView.setText(detailStrings[position]);
+
+        //For Icon
+        ImageView iconImageView = (ImageView) objView.findViewById(R.id.imageView);
+        iconImageView.setImageResource(inconInts[position]);
+
+        return objView;
+
+        return null;
+    }
+}   //Main Class
+
